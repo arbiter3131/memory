460d1e6705a831db77c7ce253b664707502f46ea
Julien Lacour <julienlacour31@gmail.com>
2020-05-18 18:22:32 +0300

    Add test file and libraries

---
+++ src/App.spec.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+
+import App from './App'
+import GuessCount from './GuessCount'
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />)
+    console.log(wrapper.getElements())
+    expect(wrapper).to.contain(<GuessCount guesses={0} />)
+  })
+
+  it('has 36 cards', () => {
+    const wrapper = shallow(<App />)
+    
+    expect(wrapper.find('Card')).to.have.length(36)
+  })
+})
---
+++ src/setupTests.js
@@ -0,0 +1,10 @@
+import chai from 'chai'
+import dirtyChai from 'dirty-chai'
+import createChaiJestDiff from 'chai-jest-diff'
+import Adapter from 'enzyme-adapter-react-16'
+import { configure as configureEnzyme } from 'enzyme'
+import createChaiEnzyme from 'chai-enzyme'
+
+chai.use(dirtyChai).use(createChaiJestDiff()).use(createChaiEnzyme);
+
+configureEnzyme({ adapter: new Adapter() })
